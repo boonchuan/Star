6906f1e3a19711531e3ab92dad697f223d34c245
Lim Boon Chuan <boonchuan@singapore.to>
2025-07-29 13:01:26 +0800

    Refresh

---
+++ blog/wp-content/themes/starfall-theme/starfall-theme/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body>
+<header>
+    <h1><a href="https://starfallcatastrophe.com">Starfall Catastrophe</a></h1>
+    <nav>
+        <ul>
+            <li><a href="/lore">Lore</a></li>
+            <li><a href="/gameplay">Gameplay</a></li>
+            <li><a href="/kickstarter">Kickstarter</a></li>
+            <li><a href="/blog">Blog</a></li>
+            <li><a href="/cards">Cards</a></li>
+            <li><a href="/newsletter">Newsletter</a></li>
+        </ul>
+    </nav>
+</header>
